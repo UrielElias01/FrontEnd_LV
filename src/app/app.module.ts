2a586d13aa6a41aca7d043f6482e2740f050a7e5
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2024-08-02 03:00:12 -0600

    Conección del Login con Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit(): void {
+    this.authService.login(this.email, this.password).subscribe(
+      response => {
+        // Manejar la respuesta del backend
+        console.log('Inicio de sesión exitoso', response);
+        this.router.navigate(['/home']); // Redirigir al usuario a la página de inicio
+      },
+      error => {
+        // Manejar errores
+        console.error('Error en el inicio de sesión', error);
+      }
+    );
+  }
+
+}
