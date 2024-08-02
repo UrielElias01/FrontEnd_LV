a9d4fce093d37b55fc3083349c60ff49e52a7a16
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2024-08-02 03:35:35 -0600

    Conexión del register al Backend

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  repassword: string = '';
+  role: String ='user';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit(): void {
+    if (this.password !== this.repassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
+    const user = {
+      email: this.email,
+      password: this.password,
+      role: this.role
+    };
+
+    this.http.post('http://localhost:3000/usuario/', user).subscribe(
+      response => {
+        console.log('Registro exitoso', response);
+        this.router.navigate(['/login']);
+      },
+      error => {
+        console.error('Error en el registro', error);
+      }
+    );
+  }
 }
